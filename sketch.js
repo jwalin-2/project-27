3bca853f1fa2b8a311682e70a44977cc37b93ef5
jwalin-2 <74650651+jwalin-2@users.noreply.github.com>
2020-12-11 20:13:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var bob
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+    bob=createSprite(250,750,20,20);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
